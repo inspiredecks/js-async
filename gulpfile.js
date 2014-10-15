d0c0edf3dcda054a9dd65b99abb355ae0c174e70
Tri Nguyen <tri@tridnguyen.com>
2014-10-15 00:31:35 -0400

    add gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var copy = require('gulp-copy');
 var del = require('del');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
 
 gulp.task('clean', function () {
 	del(['dist']);
@@ -18,6 +21,15 @@ gulp.task('copy:html', function () {
 	return gulp.src('index.html')
 		.pipe(copy('dist'));
 });
+
+gulp.task('scss', function () {
+	return gulp.src('scss/**/*.scss')
+		.pipe(sass())
+		.pipe(prefix())
+		..pipe(csso())
+		.pipe(gulp.dest('dist/style'));
+});
+
 
 gulp.task('build', ['clean', 'copy:lib', 'copy:html']);
 
